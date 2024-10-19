76d0b1251f12cce6856e1e1bde3ddfbada475f81
syytruong <syytruong@gmail.com>
2024-10-19 23:28:02 +0300

    CHORE set axios default base url

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3333';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

739ba9196b16a1d8169491cd54e7c9056ccc1e65
syytruong <syytruong@gmail.com>
2024-10-19 09:40:21 +0300

    CHORE adjust FE project structure

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import axios from 'axios';
-import Login from '../components/Login';
-import Home from '../components/Home';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 const StyledApp = styled.div`
   // Your style here

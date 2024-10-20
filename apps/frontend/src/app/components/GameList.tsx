b10398421e7c4816edbc305c06ab467811b5cb52
syytruong <syytruong@gmail.com>
2024-10-20 08:09:29 +0300

    CHORE add game name when hover

--- apps/frontend/src/app/components/GameList.tsx
+++ apps/frontend/src/app/components/GameList.tsx
@@ -19,6 +19,24 @@ const GameItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: start;
+  position: relative;
+
+  &:hover .game-name {
+    opacity: 1;
+  }
+`;
+
+const GameName = styled.div`
+  position: absolute;
+  bottom: 50px;
+  left: 30px;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  cursor: pointer;
 `;
 
 interface Game {
@@ -57,6 +75,7 @@ const GameList: React.FC<GameListProps> = ({ columns }) => {
       {games.map((game) => (
         <GameItem key={game.id}>
           <Logo src={game.logo} alt={game.name} />
+          <GameName className="game-name">{game.name}</GameName>
         </GameItem>
       ))}
     </GamesContainer>

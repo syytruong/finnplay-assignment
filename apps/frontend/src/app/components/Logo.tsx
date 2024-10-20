510d40303b88204c9b38ac036cad30fec48e2274
syytruong <syytruong@gmail.com>
2024-10-20 07:21:02 +0300

    FEAT Logo component

---
+++ apps/frontend/src/app/components/Logo.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoImage = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+interface LogoProps {
+  src: string;
+  alt: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ src, alt }) => {
+  return (
+    <LogoContainer>
+      <LogoImage src={src} alt={alt} />
+    </LogoContainer>
+  );
+};
+
+export default Logo;
